2b424d5ddf2d5813a7d4d3dc7bc5ce96f904863c
Wise-deldu <delduwise@gmail.com>
2024-10-10 18:35:14 +0000

    Create the Job creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,31 @@
+#!/usr/bin/env node
+import { createQueue } from 'kue';
+
+// Create queue
+const queue = createQueue({name: 'push_notification_code'});
+
+// Job data
+const jobData = {
+  phoneNumber: '0205034',
+  message: 'This is the code to verify your account'
+};
+
+// Create job
+const job = queue.create('push_notification_code', jobData);
+
+job
+  .on('enqueue', () => {
+    console.log(`Notification job created: ${job.id}`);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed', () => {
+    console.log('Notification job failed');
+  })
+
+  .save((error) => {
+    if (error) {
+      console.log('Error creating job:', error);
+    }
+  });
